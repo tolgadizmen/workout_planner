9ed979b1c10ca60ca97529f9d4fa5ff0cf81cec7
Tolga Dizmen <tolga@tolgadizmen.com>
2025-11-26 17:32:18 -0300

    Update exercise data

--- data.js
+++ data.js
@@ -1,42 +1,74 @@
 export const exercises = [
   {
     id: 1,
-    name: "Push-Ups",
-    sets: 3,
-    reps: 10,
-    duration: null,
-    image: "pushups_placeholder.png"
-  },
-  {
-    id: 2,
-    name: "Running",
+    name: "Light Jog",
     sets: 1,
     reps: null,
-    duration: "30:00",
+    duration: "15:00",
     image: "running_placeholder.png"
   },
   {
-    id: 3,
-    name: "Plank",
+    id: 2,
+    name: "Squat",
     sets: 3,
-    reps: null,
-    duration: "1:00",
-    image: "plank_placeholder.png"
-  },
-  {
-    id: 4,
-    name: "Squats",
-    sets: 3,
-    reps: 15,
+    reps: 10,
     duration: null,
     image: "squats_placeholder.png"
   },
   {
-    id: 5,
+    id: 3,
     name: "Lunges",
     sets: 3,
-    reps: 12,
+    reps: 8,
     duration: null,
-    image: "lunges_placeholder.png"
-  }
+    image: null
+  },
+  {
+    id: 4,
+    name: "Push-Up",
+    sets: 3,
+    reps: 8,
+    duration: null,
+    image: "pushups_placeholder.png"
+  },
+  {
+    id: 5,
+    name: "Dumbbell Rows",
+    sets: 3,
+    reps: 10,
+    duration: null,
+    image: ""
+  },
+  {
+    id: 6,
+    name: "Overhead Press",
+    sets: 3,
+    reps: 10,
+    duration: null,
+    image: ""
+  },
+  {
+    id: 7,
+    name: "Plank",
+    sets: 3,
+    reps: null,
+    duration: "00:30",
+    image: "plank_placeholder.png"
+  },
+  {
+    id: 8,
+    name: "Bicycle Crunches",
+    sets: 3,
+    reps: 10,
+    duration: null,
+    image: ""
+  },
+  {
+    id: 9,
+    name: "Stretching",
+    sets: 3,
+    reps: null,
+    duration: "01:00",
+    image: ""
+  },
 ];
